11ac29a6b8fd104d9adac87a095e53b1053ddb51
Daiderd Jordan <daiderd@gmail.com>
2017-06-25 21:16:23 +0200

    python-modules: namespace pythonPackages

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5,8 +5,13 @@ let
 in
 
 {
-  toplevel = callPackage ../development/python-modules {
-    inherit (self.python27Packages) python virtualenv;
-    pip = self.python27Packages.bootstrapped-pip;
+  toplevel = rec {
+    inherit (self) pkgs;
+    inherit (pythonPackages) virtualenvWith;
+
+    pythonPackages = callPackage ../development/python-modules {
+      inherit (self.python27Packages) python virtualenv;
+      pip = self.python27Packages.bootstrapped-pip;
+    };
   };
 }
