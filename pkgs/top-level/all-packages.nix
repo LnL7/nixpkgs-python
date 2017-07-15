9f6f0bec726ad08adfdc820237305c27b0240c92
Daiderd Jordan <daiderd@gmail.com>
2017-07-15 13:09:42 +0200

    python-packages: add attrset with wheels

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2,12 +2,18 @@ self: super:
 
 let
   inherit (self) callPackage recurseIntoAttrs;
+  inherit (self.stdenv.lib) filterAttrs mapAttrs;
+
+  getWheel = _: drv: let res = builtins.tryEval drv.wheel or {}; in if res.success then res.value else {};
+  mapWheels = attrs: filterAttrs (_: v: v != {}) (mapAttrs getWheel attrs);
 in
 
 {
   toplevel = recurseIntoAttrs rec {
     inherit (self) pkgs;
     inherit (pythonPackages) python pythonWith virtualenvWith;
+
+    pythonWheels = recurseIntoAttrs (mapWheels self.toplevel.pythonPackages);
 
     pythonPackages = recurseIntoAttrs (callPackage ../development/python-modules {
       inherit (self.python27Packages) python virtualenv;
