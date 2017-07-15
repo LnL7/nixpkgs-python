009fc70eed09d0ed66698a57ed08b5baa3bf6178
Daiderd Jordan <daiderd@gmail.com>
2017-07-15 12:26:31 +0200

    cleanup all-packages

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1,15 +1,15 @@
 self: super:
 
 let
-  inherit (super) callPackage;
+  inherit (self) callPackage recurseIntoAttrs;
 in
 
 {
-  toplevel = super.recurseIntoAttrs rec {
+  toplevel = recurseIntoAttrs rec {
     inherit (self) pkgs;
     inherit (pythonPackages) python pythonWith virtualenvWith;
 
-    pythonPackages = super.recurseIntoAttrs (callPackage ../development/python-modules {
+    pythonPackages = recurseIntoAttrs (callPackage ../development/python-modules {
       inherit (self.python27Packages) python virtualenv;
       pip = self.python27Packages.bootstrapped-pip;
     });
