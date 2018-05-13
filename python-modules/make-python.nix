ed80ba54f9995a5e24053c6da52c9cbfb5aee408
Daiderd Jordan <daiderd@gmail.com>
2018-05-13 10:44:23 +0200

    pythonng: add python.mkDerivation to pythonWithPackages

--- python-modules/make-python.nix
+++ python-modules/make-python.nix
@@ -5,23 +5,27 @@
 
 let
   pythonDepends = withPackages pythonScope;
+
+  self = buildEnv {
+    name = "${python.name}-with-packages";
+    paths = [ python ] ++ pythonDepends;
+    pathsToLink = [ "/lib" ];
+
+    postBuild = ''
+      mkdir -p $out/bin
+      python=${python}
+      for f in $python/bin/*; do
+          if [ -L $f ]; then
+              l=$(readlink -f $f)
+              ln -s ''${l/$python/$out} $out/bin/''${f##*/}
+          else
+              cp $f $out/bin
+          fi
+      done
+    '';
+  };
 in
 
-buildEnv {
-  name = "${python.name}-with-packages";
-  paths = [ python ] ++ pythonDepends;
-  pathsToLink = [ "/lib" ];
-
-  postBuild = ''
-    mkdir -p $out/bin
-    python=${python}
-    for f in $python/bin/*; do
-        if [ -L $f ]; then
-            l=$(readlink -f $f)
-            ln -s ''${l/$python/$out} $out/bin/''${f##*/}
-        else
-            cp $f $out/bin
-        fi
-    done
-  '';
+self // {
+  mkDerivation = python.mkDerivation.override { python = self; };
 }
