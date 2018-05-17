3c7a6c41a5b923e9925ee3c899a54117e52a5704
Daiderd Jordan <daiderd@gmail.com>
2018-05-17 22:58:08 +0200

    pythonng: make virtualenv use python from PATH

--- pkgs/python-modules/make-virtualenv.nix
+++ pkgs/python-modules/make-virtualenv.nix
@@ -1,39 +1,60 @@
-{ pkgs, buildEnv, mkShell, python, virtualenv, pythonPlatform, pythonScope }:
+{ stdenv, buildEnv, mkShell, python, virtualenv, pythonPlatform, pythonScope }:
 
 { withPackages
+, pythonTools ? [], pythonDepends ? []
+, systemDepends ? []
 , buildInputs ? []
 , installHook ? "", pipFlags ? ""
 , shellHook ? ""
 }:
 
 let
-  pythonDepends = withPackages pythonScope;
+  pkgs = withPackages pythonScope;
 
   env = buildEnv {
     name = "${python.name}-environment";
-    paths = pythonDepends;
+    paths = pkgs ++ pythonDepends;
+    pathsToLink = [ "/lib" ];
+
+    postBuild = ''
+      mkdir -p $out/bin
+      for dep in ${stdenv.lib.concatStringsSep " " (pkgs ++ pythonTools)}; do
+          for f in $dep/bin/*; do
+              substitute $f $out/bin/''${f##*/} \
+                  --replace '#!${python}/bin/' '#!/usr/bin/env '
+              chmod +x $out/bin/''${f##*/}
+          done
+      done
+    '';
   };
 in
 
 mkShell {
   name = "${python.name}-shell-environment";
-  inherit env pythonDepends pipFlags;
-
-  buildInputs = [ env ] ++ buildInputs;
+  inherit env pipFlags;
+  buildInputs = [ env ] ++ systemDepends ++ buildInputs;
 
   shellHook = ''
-    export PATH="$PWD/venv/bin:$PWD/venv/nix-profile/bin''${PATH:+:}$PATH"
-    if ! test -e venv/nix-profile; then
-      ${virtualenv}/bin/virtualenv venv --no-download
-      if [ -n "$pipFlags" ]; then
-        pip install $pipFlags -I --no-deps --disable-pip-version-check
-      fi
-      ${installHook}
+    prefix=$PWD/venv
+    profile=$prefix/nix-profile
+
+    addToSearchPath PATH $prefix/bin
+
+    if ! test -e $profile; then
+        ${virtualenv}/bin/virtualenv $prefix --no-download
+        substituteInPlace $prefix/bin/activate \
+            --replace '$VIRTUAL_ENV/bin' '$VIRTUAL_ENV/bin:$VIRTUAL_ENV/nix-profile/bin'
+
+        if [ -n "$pipFlags" ]; then
+            pip install $pipFlags -I --no-deps --disable-pip-version-check
+        fi
+        ${installHook}
     fi
-    if test ${env} != "$(readlink venv/nix-profile)"; then
-      rm venv/nix-profile 2> /dev/null || true
-      echo ${env}/${pythonPlatform.sitePackages} > venv/${pythonPlatform.sitePackages}/${env.name}.pth
-      nix-store -r ${env} --indirect --add-root $PWD/venv/nix-profile > /dev/null
+
+    if test ${env} != "$(readlink $profile)"; then
+        rm $profile 2> /dev/null || true
+        echo ${env}/${pythonPlatform.sitePackages} > $prefix/${pythonPlatform.sitePackages}/${env.name}.pth
+        nix-store -r ${env} --indirect --add-root $profile > /dev/null
     fi
 
     ${shellHook}
