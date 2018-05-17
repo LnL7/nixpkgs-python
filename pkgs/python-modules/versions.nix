ba4fa0fe5f7985e46e96c15033aa29cca1d26116
Daiderd Jordan <daiderd@gmail.com>
2018-05-17 23:30:16 +0200

    example packages

--- pkgs/python-modules/versions.nix
+++ pkgs/python-modules/versions.nix
@@ -1,4 +1,44 @@
 self: super:
 
 {
+  appnope = self.appnope_0_1_0;
+  asn1crypto = self.asn1crypto_0_24_0;
+  backcall = self.backcall_0_1_0;
+  backports-shutil-get-terminal-size = self.backports-shutil-get-terminal-size_1_0_0;
+  cffi = self.cffi_1_11_5;
+  cryptography = self.cryptography_2_2_2;
+  decorator = self.decorator_4_3_0;
+  distlib = self.distlib_0_2_7;
+  enum34 = self.enum34_1_1_6;
+  idna = self.idna_2_6;
+  ipaddress = self.ipaddress_1_0_22;
+  ipython-genutils = self.ipython-genutils_0_2_0;
+  ipython = self.ipython_6_4_0;
+  jedi = self.jedi_0_12_0;
+  lxml = self.lxml_4_2_1;
+  nose = self.nose_1_3_7;
+  parso = self.parso_0_2_0;
+  pathlib2 = self.pathlib2_2_3_2;
+  pbr = self.pbr_4_0_2;
+  pexpect = self.pexpect_4_5_0;
+  pickleshare = self.pickleshare_0_7_4;
+  prompt-toolkit = self.prompt-toolkit_1_0_15;
+  psutil = self.psutil_5_4_5;
+  psycopg2 = self.psycopg2_2_7_4;
+  ptyprocess = self.ptyprocess_0_5_2;
+  pyasn1-modules = self.pyasn1-modules_0_2_1;
+  pyasn1 = self.pyasn1_0_4_2;
+  pycparser = self.pycparser_2_18;
+  pygments = self.pygments_2_2_0;
+  pynacl = self.pynacl_1_2_1;
+  python-ldap = self.python-ldap_3_0_0;
+  pyyaml = self.pyyaml_3_12;
+  scandir = self.scandir_1_7;
+  simplegeneric = self.simplegeneric_0_8_1;
+  six = self.six_1_11_0;
+  setuptools = self.setuptools_39_1_0;
+  stevedore = self.stevedore_1_28_0;
+  traitlets = self.traitlets_4_3_2;
+  typing = self.typing_3_6_4;
+  wcwidth = self.wcwidth_0_1_7;
 }
