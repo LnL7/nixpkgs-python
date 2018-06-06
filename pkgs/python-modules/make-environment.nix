d368479abd16e24b9f8f074d242725210905eec9
Daiderd Jordan <daiderd@gmail.com>
2018-06-07 00:42:50 +0200

    pythonng: use symlink merge implementation from generic-builder instead of buildEnv

--- pkgs/python-modules/make-environment.nix
+++ pkgs/python-modules/make-environment.nix
@@ -1,28 +1,53 @@
-{ stdenv, buildEnv, python, pythonPlatform, pythonScope }:
+{ stdenv, python, pythonPlatform, pythonScope }:
 
-{ withPackages ? p: []
+{ name ? "${pythonPlatform.python}-environment"
+, withPackages ? p: []
 , pythonTools ? [], pythonDepends ? []
-}:
+, dontDetectConflicts ? false
+, ...
+}@attrs:
 
 let
   pkgs = withPackages pythonScope;
 in
 
-buildEnv {
-  name = "${pythonPlatform.python}-environment";
-  paths = pkgs ++ pythonDepends;
-  pathsToLink = [ "/lib" ];
+stdenv.mkDerivation (attrs // {
+  inherit name;
+  buildInputs = [ python ];
 
-  ignoreCollisions = true;
+  pythonDepends = pkgs ++ pythonDepends;
+  pythonTools = pkgs ++ pythonTools;
 
-  postBuild = ''
-    mkdir -p $out/bin
-    for dep in ${stdenv.lib.concatStringsSep " " (pkgs ++ pythonTools)}; do
+  buildCommand = ''
+    for dep in $pythonDepends; do
+        for f in $(find $dep/${pythonPlatform.sitePackages} -type f -o -type l); do
+            l=''${f/$dep/$out}
+            f=$(readlink -f "$f")
+            if [ -s "$l" ]; then
+                continue
+            fi
+            if [ -L "$l" ]; then
+                l=$(readlink -f "$f")
+                if [ "$f" = "$l" ]; then
+                    continue
+                fi
+            fi
+            mkdir -p "$(dirname "$l")"
+            ln -s "$f" "$l"
+        done
+    done
+
+    if [ -z "''${dontDetectConflicts:-}" ]; then
+        ${pythonPlatform.python} ${./pip/detect_conflicts.py} $out/${pythonPlatform.sitePackages}/*.dist-info
+    fi
+
+    for dep in $pythonTools; do
         for f in $dep/bin/*; do
+            mkdir -p $out/bin
             substitute $f $out/bin/''${f##*/} \
                 --replace '#!${python}/bin/' '#!/usr/bin/env '
             chmod +x $out/bin/''${f##*/}
         done
     done
   '';
-}
+})
